2467dd3180a19b79f528ce8d6a0076e1265b764c
Mikael De Lachevrotiere <mikdelac@hotmail.com>
2024-07-24 03:16:54 +0100

    modified mac address of new mini-1

--- pi/pyqt5_bluepy_thread.py
+++ pi/pyqt5_bluepy_thread.py
@@ -39,7 +39,7 @@ class WorkerBLE(QRunnable):
         self.signals.signalMsg.emit("WorkerBLE start")
         
         #---------------------------------------------
-        p = btle.Peripheral("7C:87:CE:30:6D:12")
+        p = btle.Peripheral("08:F9:E0:20:3E:0A")
         p.setDelegate( MyDelegate(self.signals) )
 
         svc = p.getServiceByUUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
